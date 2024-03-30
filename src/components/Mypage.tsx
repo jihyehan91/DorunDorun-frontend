b76953b30e8c5a274949c61b12986dc5c1515d5d
ghtjd1358 <hojjang18@naver.com>
2024-03-30 13:50:14 +0900

    찐막

--- src/components/Mypage.tsx
+++ src/components/Mypage.tsx
@@ -59,13 +59,13 @@ export default function Mypage() {
   const handleWithdraw = async (userdata: FormData) => {
     console.log('이것은 userdata', userdata);
     console.log('이것은 userdata의 userId', userdata.userId);
-
     const confirmdraw = window.confirm('정말로 회원탈퇴를 하시겠습니까?');
     if (confirmdraw) {
       try {
         const response = await axios.delete(`${API_URL}/user/withdraw`, {
-          params: {
-            userId: userdata.userId
+          data: { userId: userdata.userId }, 
+          headers: {
+            'Content-Type': 'application/json' 
           },
           withCredentials: true,
         });
@@ -77,11 +77,14 @@ export default function Mypage() {
         console.error('에러:', error);
       }
     }
-};
-
+  };
+  
+  const onSubmit = () => {
+    navigate('/')
+  }
 
   // 사용자 정보 수정
-  const onSubmit: SubmitHandler<FormData> = async (userdata) => {
+  const emailChangeHandler: SubmitHandler<FormData> = async (userdata) => {
     const requestData = {
       userid: userdata.userId,
       email: userdata.email,
@@ -92,70 +95,49 @@ export default function Mypage() {
         const response = await axios.patch(`${API_URL}/user/changeEmail`, requestData, {
           withCredentials: true,
           headers: {
-            'Content-Type': 'application/json' // 수정된 Content-Type
+            'Content-Type': 'application/json' 
           }
         });
         console.log('이메일 변경:', response.data);
         if (response.data.result === false) {
           alert(response.data.msg);
         } else {
-          navigate(`/`);
           alert('이메일이 변경되었습니다');
         }
         return response.data;
-      } else if (userdata.password) {
+      } 
+    } catch (error) {
+      console.error('에러:', error);
+    }
+  };
+
+  const passwordChangeHandler: SubmitHandler<FormData> = async (userdata) => {
+    const requestData = {
+      userid: userdata.userId,
+      email: userdata.email,
+      inputpw: userdata.password
+    };
+  
+    if (userdata.password) { // if문 밖으로 이동
+      try {
         const response = await axios.patch(`${API_URL}/user/changePW`, requestData, {
           withCredentials: true,
           headers: {
-            'Content-Type': 'application/json' // 수정된 Content-Type
+            'Content-Type': 'application/json'
           }
         });
         console.log('비밀번호 변경:', response.data);
         if (response.data.result === false) {
           alert(response.data.msg);
         } else {
-          navigate(`/`);
           alert('비밀번호가 변경되었습니다');
         }
         return response.data;
+      } catch (error) {
+        console.error('에러:', error);
       }
-    } catch (error) {
-      console.error('에러:', error);
-    }
+    } 
   };
-  
-
-
-  // 프로필 이미지 변경
-  // const handleImageChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
-  //   if (e.target.files) {
-  //     const formData = new FormData();
-  //     formData.append('image', e.target.files[0]);
-  
-  //     try {
-  //       const response = await axios.post(`${API_URL}/upload`, 
-  //       formData,
-  //       { withCredentials: true } ,
-  //       { 
-  //         headers: {
-  //           'Content-Type': 'multipart/form-data'
-  //         }
-  //       });
-  
-  //       const imageUrl = response.data.imageUrl;
-  //       setGetUser(prevState => ({
-  //         ...prevState,
-  //         profileImage: imageUrl
-  //       }));
-  //     } catch (error) {
-  //       console.error('이미지 업로드 에러:', error);
-  //     }
-  //   }
-  // };
- 
-  // useEffect(() => {
-  //   console.log('프로필 이미지가 업데이트되었습니다:', getUser.profileImage)
-  // }, [getUser.profileImage]);
 
   return (
     <div className='form-container'>
@@ -167,24 +149,9 @@ export default function Mypage() {
             </Link>
           </div>
           {
-            userCheck ?
+            // userCheck ?
             <div className='form-box'>
             <form className='auth-form' onSubmit={handleSubmit(onSubmit)}>
-              {/* <div>
-                <img src={getUser.profileImage} alt='프로필 이미지' />
-              </div> */}
-
-              {/* <label className='auth-label' htmlFor='username'>
-                이름
-              </label>
-              <input
-                className='auth-input'
-                value={getUser.username}
-                type='text'
-                id='username'
-                disabled
-              /> */}
-
               <label className='auth-label' htmlFor='userId'>
                 아이디
               </label>
@@ -199,80 +166,84 @@ export default function Mypage() {
               <label className='auth-label' htmlFor='email'>
                 이메일
               </label>
-              <input
-                className='auth-input'
-                type='email'
-                id='email'
-                placeholder='이메일을 변경해주세요'
-                {...register('email', {
-                  required: '이메일을 입력해주세요',
-                  pattern: {
-                    value: /\S+@\S+\.\S+/,
-                    message: '유효한 이메일 주소를 입력하세요'
-                  }
-                })}
-              />
+              <div className='flex'>
+                <input
+                  className='auth-input'
+                  type='email'
+                  id='email'
+                  placeholder='이메일을 변경해주세요'
+                  {...register('email', {
+                    required: '이메일을 입력해주세요',
+                    pattern: {
+                      value: /\S+@\S+\.\S+/,
+                      message: '유효한 이메일 주소를 입력하세요'
+                    }
+                  })}
+                />
+                <button
+                  className='py-0.5 rounded border-0 text-white font-medium text-sm bg-[var(--btn-bg)] ml-1'
+                  type='button' 
+                  onClick={emailChangeHandler}>이메일 변경</button>
+              </div>
               {errors.email && (
                 <span className='authSpan' role='alert'>
                   {errors.email.message}
                 </span>
               )}
 
+              
               <label className='auth-label' htmlFor='password'>
                 비밀번호
               </label>
-              <input
-                className='auth-input'
-                type='password'
-                id='password'
-                placeholder="'영문, 숫자, 대문자, 특수문자 포함 8자리 이상'"
-                {...register('password', {
-                  required: '비밀번호를 입력하세요',
-                  minLength: {
-                    value: 8,
-                    message: '비밀번호는 8자 이상이어야 합니다'
-                  },
-                  validate: (value) => {
-                    const pwNumber = /\d/.test(value);
-                    const pwUpperCase = /[A-Z]/.test(value);
-                    const pwLowerCase = /[a-z]/.test(value);
-                    const pwSpecialChar = /[!@#$%^&*]/.test(value);
-                    return (
-                      pwNumber &&
-                      pwUpperCase &&
-                      pwLowerCase &&
-                      pwSpecialChar ||
-                      '비밀번호는 숫자, 대문자, 소문자, 특수문자를 포함해야 합니다'
-                    );
-                  }
-                })}
-              />
+              <div className='flex'>
+                <input
+                  className='auth-input'
+                  type='password'
+                  id='password'
+                  placeholder="'영문, 숫자, 대문자, 특수문자 포함 8자리 이상'"
+                  {...register('password', {
+                    required: '비밀번호를 입력하세요',
+                    minLength: {
+                      value: 8,
+                      message: '비밀번호는 8자 이상이어야 합니다'
+                    },
+                    validate: (value) => {
+                      const pwNumber = /\d/.test(value);
+                      const pwUpperCase = /[A-Z]/.test(value);
+                      const pwLowerCase = /[a-z]/.test(value);
+                      const pwSpecialChar = /[!@#$%^&*]/.test(value);
+                      return (
+                        pwNumber &&
+                        pwUpperCase &&
+                        pwLowerCase &&
+                        pwSpecialChar ||
+                        '비밀번호는 숫자, 대문자, 소문자, 특수문자를 포함해야 합니다'
+                      );
+                    }
+                  })}
+                />
+                <button
+                  className='py-0.5 rounded border-0 text-white font-medium text-sm bg-[var(--btn-bg)] ml-1'
+                  type='button' 
+                  onClick={passwordChangeHandler}
+                >비밀번호 변경</button>
+              </div>
               {errors.password && (
                 <span className='authSpan' role='alert'>
                   {errors.password.message}
                 </span>
               )}
 
-              {/* <label className='auth-label' htmlFor='profileImage'>
-                프로필 업로드
-              </label>
-              <input
-                className='auth-input'
-                type='file'
-                id='profileImage'
-                onChange={handleImageChange}
-                accept='image/*'
-              /> */}
               <button className='auth-input mt-11' type='submit'>
-                수정하기
+                홈으로 돌아가기
               </button>
               <p onClick={handleWithdraw} className='auth-span font-black opacity-60 mb-2 text-right cursor-pointer' role='alert'>
-        회원탈퇴
-      </p>
+                회원탈퇴
+              </p>
             </form>
           </div> 
-          :
-          <Notfound/>
+          // :
+          // <Notfound/>
           }
         </div>
       </div>
