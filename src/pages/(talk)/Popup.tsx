59b2ae709c76d8216a05b7f17db34f51780f9baf
roo2bos <roo2bos@gmail.com>
2024-03-30 14:38:48 +0900

    미션 : 없을때 메세지 추가 및 UI 수정

--- src/pages/(talk)/Popup.tsx
+++ src/pages/(talk)/Popup.tsx
@@ -4,7 +4,6 @@ import { IoMdCloseCircle } from 'react-icons/io';
 import { useNavigate } from 'react-router-dom';
 
 export const Popup = ({ title, allMsg, datas, isPop, setIsPop }) => {
-
 	const navigate = useNavigate();
 	const [mssCount, setMssCount] = useState(0);
 	const [authUser, setAuthUser] = useState({});
@@ -14,7 +13,8 @@ export const Popup = ({ title, allMsg, datas, isPop, setIsPop }) => {
 		if (completedCount === 3 && mssCount < 1) {
 			setMssCount((prevMss) => prevMss + 1);
 		}
-    async function auth() {//지못미;
+		async function auth() {
+			//지못미;
 			const res = await axios.get('https://43.203.227.36.sslip.io/server/user/authuser');
 			const result = res.data;
 			setAuthUser(result);
@@ -27,28 +27,26 @@ export const Popup = ({ title, allMsg, datas, isPop, setIsPop }) => {
 			// });
 		}
 		auth();
-	}, [datas, mssCount,completedCount]);
+	}, [datas, mssCount, completedCount]);
 
-
-  // function historySave(allmessage) {
-  //   console.log(allmessage);
-  //   localStorage.setItem('_doRunChatHistory', JSON.stringify(allmessage));
-  // }
-  function closeHandler(){
-
-      console.log(allMsg);
-      if(title === '교정 목록' && completedCount >= 3){
-        if(authUser.result){
-          if(!confirm('복습하기 페이지로 이동하시겠습니까?')) return;
-          // historySave(allMsg); //서버 저장
-          navigate(`/mylog`);
-        } else{
-          if(!confirm('학습된 기록은 저장되지 않습니다. 회원가입 후 이용 해주세요.')) return;
-          navigate(`/signup`);
-        }
-      }
-      setIsPop(false);
-  }
+	// function historySave(allmessage) {
+	//   console.log(allmessage);
+	//   localStorage.setItem('_doRunChatHistory', JSON.stringify(allmessage));
+	// }
+	function closeHandler() {
+		console.log(allMsg);
+		if (title === '교정 목록' && completedCount >= 3) {
+			if (authUser.result) {
+				if (!confirm('복습하기 페이지로 이동하시겠습니까?')) return;
+				// historySave(allMsg); //서버 저장
+				navigate(`/mylog`);
+			} else {
+				if (!confirm('학습된 기록은 저장되지 않습니다. 회원가입 후 이용 해주세요.')) return;
+				navigate(`/signup`);
+			}
+		}
+		setIsPop(false);
+	}
 	function List({ title }) {
 		switch (title) {
 			case '교정 목록':
@@ -68,18 +66,24 @@ export const Popup = ({ title, allMsg, datas, isPop, setIsPop }) => {
 			case '오늘의 학습 미션':
 				return (
 					<>
-						<p className="todo">
+						{/* <p className="todo">
 							<span>오늘의 목표: 미달({datas.filter((data) => data.complete).length}/3)</span>{' '}
 							<span>전체 목표량 : {mssCount}/30일</span>
-						</p>
+						</p> */}
 						<ul className="list-mission">
-							{datas.map((mission) => {
-								return (
-									<li key={mission.id} className={`${mission.complete ? 'complete' : ''}`}>
-										<span>{mission.message}</span>
-									</li>
-								);
-							})}
+							{
+								(datas.length === 0 ? (
+									<li className="nodata">등록된 미션이 없습니다</li>
+								) : (
+									datas.map((mission) => {
+										return (
+											<li key={mission.id} className={`${mission.complete ? 'complete' : ''}`}>
+												<span>{mission.message}</span>
+											</li>
+										);
+									})
+								))
+							}
 						</ul>
 					</>
 				);
@@ -105,9 +109,7 @@ export const Popup = ({ title, allMsg, datas, isPop, setIsPop }) => {
 				<div className="ly-inner">
 					<div className="ly-head">
 						<strong>{title}</strong>
-						<button
-							type="button"
-							onClick={()=>closeHandler(authUser)}>
+						<button type="button" onClick={() => closeHandler(authUser)}>
 							<IoMdCloseCircle className="text-[var(--highlight-color)]" />
 						</button>
 					</div>
