ce6a4537822718f30aef9c5a43e5e51a64968e7f
jihyehan91 <toto5523@hanmail.net>
2024-04-01 13:20:21 +0900

    수정수정

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -23,7 +23,7 @@ function Home() {
             <h3>Let's chat</h3>
             <p className='banner-text'>안녕, 나와 대화해볼래?</p>
             <p className='banner-text'>우리 함께 즐거운 영어 공부하자!</p>
-            <Link to='/chat' className='banner-link-chat'>
+            <Link to='/chat' className='banner-link'>
               지금 대화하러 가기
             </Link>
           </div>
@@ -35,30 +35,37 @@ function Home() {
       </div>
       {/* 추천 캐릭터 */}
       <div className='main-character'>
-        <h2 className='list-title main-title'>추천 대화</h2>
-        <p className='mb-10 text-[var(--sub-font-color)] text-lg'>
+        <h2 className='list-title main-title'>캐릭터랑 대화하기</h2>
+        <p className='main-list-describe'>
           친구들이 당신을 기다리고 있어요{`:)`}
         </p>
         <CharacterList data={recomm} />
       </div>
       {/* 예문, 퀴즈 */}
       <div className='main-learn flex'>
-        <div>
+        <div className='main-preview-banner'>
           <h2 className='list-title main-title'>새로운 표현을 배워볼까요?</h2>
-          <p className='text-[var(--sub-font-color)] text-lg'>
+          <p className='main-list-describe'>
             하루에 딱 3문장만 공부하면 실력이 UP!
           </p>
-          <div>
-            <div>예문학습</div>
-          </div>
+          <Link to='/learning' className='banner-link'>
+            지금 학습하러 가기
+          </Link>
         </div>
         <div>
           <h2 className='list-title main-title'>퀴즈 풀고 실력 높이기!</h2>
-          <p className='text-[var(--sub-font-color)] text-lg mb-10'>
+          <p className='main-list-describe'>
             문장 완성하는 퀴즈를 풀며 즐겁게 공부해요~
           </p>
-          <div>
-            <Wordquiz />
+          <div className='flex bg-[var(--highlight-color)]'>
+            <div>
+              <Link to='/chat' className='banner-link'>
+                지금 대화하러 가기
+              </Link>
+            </div>
+            <div>
+              <img src='/quiz-ai.png' alt='' />
+            </div>
           </div>
         </div>
       </div>
